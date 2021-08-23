ede001e3b00376eac85bac6a6d606cd0aafea945
시혁 박 <exon.indiv@gmail.com>
2021-08-23 21:37:44 +0900

    Add: header, main

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png'
+import box from './images/box.png'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <img src={logo} />
+            </div>
+            <div className="main">
+                <h1>Skyblock Minions Rank</h1>
+                <h2>Easy Skyblock Minions Rank</h2>
+                <img src={box} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
